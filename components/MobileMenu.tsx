6071ad524c44525acc86181a6a9ec39633542a23
Gurkan Tongel <gurkantngl@gmail.com>
2024-02-24 22:01:08 +0300

    navbar

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React from 'react'
 
-interface mobilProp{
-    visible?:boolean;
+interface MobileMenuProps {
+    visible?: boolean;
+  }
 
-}
-
-const MobileMenu: React.FC<mobilProp> = ({visible}) => {
+const MobileMenu: React.FC<MobileMenuProps> = ({ visible }) => {
     if(!visible){
+
         return null;
     }
-    return (
-
-        <div className='bg-black w-48 absolute top-8 left-0 py-5 flex-col border-gray-400 flex'>
-            <div className="flex flex-col">
-
+  return (
+    <div className=
+    'bg-gray-950 w-48 absolute rounded-lg top-8 border-2 left-0 py-5 flex-col border-gray-800 flex'>
+        <div className='flex flex-col gap-4'>
+            <div className='px-3 text-center text-white hover:underline'>
+                Home
             </div>
+            <div className='px-3 text-center text-white hover:underline'>
+                Film
+            </div>
+            <div className='px-3 text-center text-white hover:underline'>
+                New & Popular
+            </div>
+            <div className='px-3 text-center text-white hover:underline'>
+                My List
+            </div>
+            <div className='px-3 text-center text-white hover:underline'>
+                Browse by Langugaes
+            </div>
+
         </div>
-    )
+     </div>
+  )
 }
 
 export default MobileMenu
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import styles from "@/styles/Home.module.css";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
 import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,12 +30,8 @@ export default function Home() {
   const {data:user} = useCurrentUser();
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <p className="text-white">{user?.email}</p>
-      <button onClick={()=>signOut()} className='w-full bg-black text-white text-xl'>Logout</button>
+    <><Navbar />
     </>
+    
   );
 }
